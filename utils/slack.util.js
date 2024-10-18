01b809a93871d9b569bb84ea74f3a65a8acf2913
Manish Kumar Pandit <manishkr9900@gmail.com>
2024-10-18 19:09:42 +0530

    slack push notification demo

---
+++ utils/slack.util.js
@@ -0,0 +1,47 @@
+const { WebClient } = require('@slack/web-api');
+
+const options = {};
+const web = new WebClient(process.env.SLACK_TOKEN, options);
+
+const sendSlackMessage = async (message, channel = null) => {
+    return new Promise(async (resolve, reject) => {
+        const channelId = channel || process.env.SLACK_CHANNEL_ID;
+        try {
+            const resp = await web.chat.postMessage({
+                text: message,
+                channel: channelId,
+            });
+            return resolve(true);
+        } catch (error) {
+            const errorMessage = error?.data?.error;
+            if (errorMessage == 'not_in_channel') {
+                console.log('not in channel')
+                // await joinSlackChannel(channelId, message);
+            }
+            return resolve(true);
+        }
+    });
+};
+
+const joinSlackChannel = (channel, message = null) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const resp = await web.conversations.join({
+                channel: channel,
+            });
+            console.log(resp);
+            if (message) {
+                await sendSlackMessage(message, channel);
+            }
+            return resolve(true);
+        } catch (error) {
+            console.log(JSON.stringify(error));
+            return resolve(true);
+        }
+    });
+};
+
+module.exports = {
+    sendSlackMessage,
+    joinSlackChannel
+};
